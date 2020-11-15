d8fa6e42d8a3a8dc7d2937fe1fa4f8aae3204c37
Sean Beaudoin <s10_n@mailbox.org>
2020-11-15 10:17:44 -0800

    annotated code with comments and added minimum player count

--- bot.py
+++ bot.py
@@ -12,30 +12,34 @@ class Player:
         self.current_score = current_score
         self.is_my_turn = is_my_turn
 
+        
 bot = commands.Bot(command_prefix='!')
 print('Huck the Hogs successfully initiated.')
 
-current_player = 0
-players_can_join = False
-game_initialized = False
-game_on = False
-players = []
+# set game variables to defaults
+game_initialized = False # has a game been initialized
+players_can_join = False # is the game in the join stage
+game_on = False # is there a game active right now
+players = [] # list of players that have joined as Player objects
+current_player = 0 # the list index of the player whose turn it is
 
-# initialize the game
-@bot.command(name='h') # adjust to !huckthehogs when complete
+# TODO: adjust to !huckthehogs when complete
+@bot.command(name='h') # initialize the game
 async def init_game(ctx):
-    global game_on
     global game_initialized
+    global game_on
     if not game_on and not game_initialized:
         global players
         global players_can_join
-        game_initialized = True
-        players_can_join = True
-        players = []
+        game_initialized = True # initialize the game
+        players_can_join = True # start the game's join stage
+        players = [] # reset the list of players
         print('Game initialized.')
-        embed_var = discord.Embed(title='Welcome to Huck the Hogs!',description='Use the **!join** command to join the game.\nUse **!quit** at any time to exit the game.')
+        embed_var = discord.Embed(title='Welcome to Huck the Hogs!',
+                                  description='Use the **!join** command to join the game.\nUse **!quit** at any time to exit the game.')
         await ctx.send(embed=embed_var)
 
+# quit the game
 @bot.command(name='quit')
 async def quit_game(ctx):
     global current_player
@@ -43,33 +47,35 @@ async def quit_game(ctx):
     global game_initialized
     global game_on
     global players
-    current_player = 0
-    players_can_join = False
+    # reset all game values to the defaults
+    current_player = 0 
+    players_can_join = False 
     game_initialized = False
     game_on = False
     players = []
-    embed_var = discord.Embed(title='Game exited.',description='Thanks for playing!')
+    embed_var = discord.Embed(title='Game exited.',
+                              description='Thanks for playing!')
     await ctx.send(embed=embed_var)
     
-        # allow players to join the game
+# allow players to join the game
 @bot.command(name='join')
 async def join_game(ctx):
     global players_can_join
     global players
-    if players_can_join:
+    if players_can_join: # ensure that the game is in the join phase
         user_ID = format(ctx.author.id)
-        if not any(player.name == user_ID for player in players):         
+        if not any(player.name == user_ID for player in players): # check if the player who is trying to join has joined already
             players.append(Player(user_ID,0,0,False))
-            if len(players) > 1:
+            print(f'{ctx.author} has joined the game.')
+            if len(players) > 1: # check if the player is the first to join or not
                 embed_var = discord.Embed(description=f'<@{ctx.author.id}> has joined the game.\n{len(players)} players so far.\n\nUse **!start** to begin when all players have joined.')
             else:
                 embed_var = discord.Embed(description=f'<@{ctx.author.id}> has joined the game.\n{len(players)} player so far.\n\nUse **!start** to begin when all players have joined.')
-        else:
+        else: # 
             embed_var = discord.Embed(description=f'<@{ctx.author.id}> has already joined the game!\n\nUse **!start** to begin when all players have joined.')
-            print(players)
         await ctx.send(embed=embed_var)
 
-
+# start the game once all players have joined
 @bot.command(name='start')
 async def start_game(ctx):
     global players_can_join
@@ -79,16 +85,20 @@ async def start_game(ctx):
     list_of_players = []
     for player in players:
         list_of_players.append(player.name)
-    if roller_ID in list_of_players and not game_on:
-        game_on = True
-        players_can_join = False
-        print('Game started')
-        embed_var = discord.Embed(title="Let's huck some hogs!",description=f"<@{players[0].name}> goes first.\n Use **!roll** to roll the pigs.")
-        global current_player
-        players[current_player].is_my_turn = True
+    if roller_ID in list_of_players and not game_on: # check that the player who issued the command is actually playing
+        if len(players) < 2:
+            embed_var = discord.Embed(description="Huck the Hogs requires at least two players.")
+        else:
+            game_on = True # start the game
+            players_can_join = False # end the joining stage
+            print('Game started')
+            embed_var = discord.Embed(title="Let's huck some hogs!",
+                                      description=f"<@{players[0].name}> goes first.\n Use **!roll** to roll the pigs.")
+            global current_player
+            players[current_player].is_my_turn = True # set the first player to join as the current player
         await ctx.send(embed=embed_var)
 
-
+# roll the pigs
 @bot.command(name='roll')
 async def player_roll(ctx):
     global current_player
@@ -100,52 +110,66 @@ async def player_roll(ctx):
             #set that player as the current player
             print(f"{player.name}'s turn.")
             embed_var = discord.Embed(description=f"<@{player.name}>'s turn.")
-            initial_roll = score.scoring(pig_roll.pig_roll(),pig_roll.pig_roll())
+            roll = score.scoring(pig_roll.pig_roll(),pig_roll.pig_roll()) # roll two pigs and return a name and a score for the roll
 
             # Player rolls a Pig Out
-            if initial_roll['name'] == 'Pig Out':
-                player.is_my_turn = False
-                player.current_score = 0
-                embed_var.add_field(name=f"{initial_roll['name']}",value=f"Total score: {player.total_score} points")
-                if current_player == (len(players) - 1):
+            if roll['name'] == 'Pig Out':
+                player.is_my_turn = False # end the player's turn
+                player.current_score = 0 # reset the player's score for that turn
+                embed_var.add_field(name=f"{roll['name']}",
+                                    value=f"Total score: {player.total_score} points")
+                # move to the next player
+                if current_player == (len(players) - 1): 
                     current_player = 0
                 else:
                     current_player += 1
-                players[current_player].is_my_turn = True
-                embed_var.add_field(name='Next player:',value=f"<@{players[current_player].name}>",inline=False)
+                players[current_player].is_my_turn = True # set the next player's turn
+                embed_var.add_field(name='Next player:',
+                                    value=f"<@{players[current_player].name}>",
+                                    inline=False)
 
             # Player rolls an Oinker
-            elif initial_roll['name'] == 'Oinker':
-                player.is_my_turn = False
-                player.current_score = 0
-                player.total_score = 0
-                embed_var.add_field(name=f"{initial_roll['name']}",value=f"Total score: {player.total_score} points")
+            elif roll['name'] == 'Oinker':
+                player.is_my_turn = False # end the player's turn
+                player.current_score = 0 # reset the player's score for that turn
+                player.total_score = 0 # reset the player's score for the entire game
+                embed_var.add_field(name=f"{roll['name']}",
+                                    value=f"Total score: {player.total_score} points")
+                # move to the next player
                 if current_player == (len(players) - 1):
                     current_player = 0
                 else:
                     current_player += 1
-                players[current_player].is_my_turn = True
-                embed_var.add_field(name='Next player:',value=f"<@{players[current_player].name}>",inline=False)
+                players[current_player].is_my_turn = True # set the next player's turn
+                embed_var.add_field(name='Next player:',
+                                    value=f"<@{players[current_player].name}>",
+                                    inline=False)
 
             # Player rolls anything else
             else:
-                player.current_score += initial_roll['score']
-                print(player.current_score)
-                embed_var.add_field(name=f"{initial_roll['name']} [+ {initial_roll['score']} points]",value=f"Score this turn: {player.current_score} points\nTotal score: {player.total_score + player.current_score} points")
-                if player.current_score + player.total_score >= 100:
-                    embed_var.add_field(name='You win!',value='Use **!huckthehogs** to play again.',inline=False)
+                player.current_score += roll['score'] # add the score from the roll
+                embed_var.add_field(name=f"{roll['name']} [+ {roll['score']} points]",
+                                    value=f"Score this turn: {player.current_score} points\nTotal score: {player.total_score + player.current_score} points")
+                if player.current_score + player.total_score >= 100: # check if player has won the game
+                    embed_var.add_field(name='You win!',
+                                        value='Use **!huckthehogs** to play again.',
+                                        inline=False)
                     global players_can_join
                     global game_initialized
                     global game_on
+                    # reset game variables
                     current_player = 0
                     players_can_join = False
                     game_initialized = False
                     game_on = False
                     players = []
                 else:
-                    embed_var.add_field(name='\u200b',value='Use **!roll** to roll again or **!pass** to move to the next player.',inline=False)
+                    embed_var.add_field(name='\u200b', # prompt the player for their next action
+                                        value='Use **!roll** to roll again or **!pass** to move to the next player.',
+                                        inline=False) 
             await ctx.send(embed=embed_var)
 
+# player ends their turn and keeps their score
 @bot.command(name='pass')
 async def player_pass(ctx):
     global current_player
@@ -155,31 +179,33 @@ async def player_pass(ctx):
     for player in players:
         if player.name == roller_ID and player.is_my_turn:
             embed_var = discord.Embed()
-            player.is_my_turn = False
-            player.total_score += player.current_score
-            embed_var.add_field(name=f"Passed",value=f"Turn score: {player.current_score} points\nTotal score: {player.total_score} points")
-            player.current_score = 0
+            player.is_my_turn = False # end the player's turn
+            player.total_score += player.current_score # add their turn's score onto their total score
+            embed_var.add_field(name=f"Passed",
+                                value=f"Turn score: {player.current_score} points\nTotal score: {player.total_score} points")
+            player.current_score = 0 # reset their current score
+            # move to the next player
             if current_player == (len(players) - 1):
                 current_player = 0
             else:
                 current_player += 1
-            players[current_player].is_my_turn = True
-            embed_var.add_field(name='Next player:',value=f"<@{players[current_player].name}>",inline=False)
+            players[current_player].is_my_turn = True # set the next player as the current player
+            embed_var.add_field(name='Next player:',
+                                value=f"<@{players[current_player].name}>",
+                                inline=False)
             await ctx.send(embed=embed_var)
 
+# show the game's current score
 @bot.command(name='score')
 async def show_score(ctx):
-    if game_on:
+    if game_on: # check that a game is taking place
         global players
         score_list = []
         for player in players:
-            score_list.append({'name':player.name,'score':player.total_score})
-
-        def get_score(player_score):
-            return player_score.get('score')
-        score_list.sort(key=get_score,reverse=True)           
-
-        scoreboard_text = ''      
+            score_list.append({'name':player.name,'score':player.total_score}) # create a list of dictionaries with each player's name and total score
+        score_list.sort(key=(lambda player_score: player_score.get('score')), # sort the list by score in descending order
+                        reverse=True)           
+        scoreboard_text = '' # create the string that holds the scoreboard text
         for player_score in score_list:
             scoreboard_text += f"<@{player_score['name']}>: {player_score['score']} points\n"
         embed_var = discord.Embed()
